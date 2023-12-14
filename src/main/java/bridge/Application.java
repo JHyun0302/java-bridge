f5db7bbc76b41849462677ac54fe1d50598c9a92
JHyun0302 <wogustkfkd11@gmail.com>
2023-12-14 14:24:07 +0900

    :sparkles: [Feat] Application

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,18 @@
 package bridge;
 
-public class Application {
+import bridge.config.Configuration;
+import bridge.controller.BridgeController;
+import bridge.view.InputView;
+import bridge.view.OutputView;
 
+public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Configuration configuration = new Configuration();
+        InputView inputView = configuration.createInputView();
+        OutputView outputView = new OutputView();
+        BridgeNumberGenerator generator = new BridgeRandomNumberGenerator();
+        BridgeController bridgeController = new BridgeController(inputView, outputView, generator);
+
+        bridgeController.run();
     }
 }
